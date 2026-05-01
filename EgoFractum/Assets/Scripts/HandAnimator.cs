7137e4edb3d32607677abbf295f0a69ef08beaec
Kunz <39652769+Duartefon@users.noreply.github.com>
2026-05-01 19:13:02 +0100

    hands working

---
+++ EgoFractum/Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+
+public class HandAnimator : MonoBehaviour
+{
+    
+    public InputActionReference gripPressAction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator handAnimator;
+    public Dictionary<String, HandPose> HandPoses = new Dictionary<string, HandPose>
+    {
+        { "Grip", null },
+        { "Pointing", null },
+        { "Idle", null },
+        
+
+    };
+    
+    public enum Fingers
+    {
+        Thumb,
+        Index,
+        Middle,
+        Ring,
+        Pinky
+    }
+
+
+
+    private void Update()
+    {
+ 
+            foreach (string finger in Enum.GetNames(typeof(Fingers)))
+            {
+                if (gripPressAction.action.ReadValue<float>() != 0)
+                {
+                    handAnimator.SetFloat(finger, gripPressAction.action.ReadValue<float>());
+                    continue;
+                }
+                handAnimator.SetFloat(finger, 0);
+               
+                
+            }
+
+  
+
+    }
+}
---
+++ EgoFractum/Assets/Scripts/HandPose.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HandPose", menuName = "Scriptable Objects/HandPose")]
+public class HandPose : ScriptableObject
+{
+
+    
+    public int[] handPose = new int[5];
+
+ 
+}
