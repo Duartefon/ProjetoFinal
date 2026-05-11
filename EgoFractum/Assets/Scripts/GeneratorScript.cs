c5933ec3093e422739e19879040517a443eff2d3
Gabriel <gabrielrei2112@gmail.com>
2026-05-11 20:44:14 +0100

    Generator Texture

--- EgoFractum/Assets/Scripts/GeneratorScript.cs
+++ EgoFractum/Assets/Scripts/GeneratorScript.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 public class GeneratorScript : MonoBehaviour
 {
     private int fuseCount = 2;
+    public bool isOn = false;
+    private AudioSource audioSource;
     private GameObject fuseTransform;
 
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isOn)
+        {
+            audioSource.Play();
+        }
     }
 
     public void AddFuse()
