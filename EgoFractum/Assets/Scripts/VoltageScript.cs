fe5ac5ca6f4203d99267c764795113d1814b1844
Gabriel <gabrielrei2112@gmail.com>
2026-05-12 22:18:14 +0100

    sound effect to voltage

--- EgoFractum/Assets/Scripts/VoltageScript.cs
+++ EgoFractum/Assets/Scripts/VoltageScript.cs
@@ -20,17 +20,36 @@ public class VoltageScript : MonoBehaviour
     public float lowVoltage = 100f;
     public float pulseFrequency = 6f;
 
+    [Header("Sound Effects")]
+    public AudioSource audioSource;
+    public AudioClip beepSound;
+    public AudioClip beepSuccessSound;
+
     [Range(0, 1000)]
     public float voltage = 0f;
-    
+    private float previousVoltage;
+
     void Start()
     {
         voltageText.color = lowVoltageColor;
         voltageText.text = $"{voltage}V";
         screenLight.color = lowVoltageColor;
+
+        previousVoltage = voltage;
     }
     void Update()
     {
+
+        if (voltage != previousVoltage)
+        {
+            if (voltage == goodVoltage)
+                BeepSuccess();
+            else
+                Beep();
+
+            previousVoltage = voltage;
+        }
+
         Color targetColor;
         if (voltage <= goodVoltage)
         {
@@ -58,5 +77,21 @@ public class VoltageScript : MonoBehaviour
         screenLight.intensity = Mathf.Clamp(
             0.5f + 0.5f * Mathf.Sin(Time.time * pulseFrequency),
             0, maxLightIntensity);
+    }
+
+    void Beep()
+    {
+        if (audioSource && beepSound)
+        {
+            audioSource.PlayOneShot(beepSound);
+        }
+    }
+
+    void BeepSuccess()
+    {
+        if (audioSource && beepSuccessSound)
+        {
+            audioSource.PlayOneShot(beepSuccessSound);
+        }
     }
 }
