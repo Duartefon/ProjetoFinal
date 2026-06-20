1680b051983689c46b017008cbb32fd00aac9557
Kunz <39652769+Duartefon@users.noreply.github.com>
2026-06-20 21:30:17 +0100

    Fix: HandAnimator Index Finger n PokeInteractor
    Fixed index mask

--- EgoFractum/Assets/Scripts/Hands/HandAnimator.cs
+++ EgoFractum/Assets/Scripts/Hands/HandAnimator.cs
@@ -11,6 +11,10 @@ public class HandAnimator : MonoBehaviour
 {
     
     public InputActionReference gripPressAction;
+
+    public InputActionReference triggerPressAction;
+    public InputActionReference thumbPressAction;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Animator handAnimator;
     public Dictionary<String, HandPose> HandPoses = new Dictionary<string, HandPose>
@@ -36,13 +40,29 @@ public class HandAnimator : MonoBehaviour
     private void Update()
     {
  
-            foreach (string finger in Enum.GetNames(typeof(Fingers)))
+            foreach (var finger in Enum.GetNames(typeof(Fingers)))
             {
+                if (finger is nameof(Fingers.Thumb))
+                {
+                    Debug.Log($"I'm pressing trigger {triggerPressAction.action.ReadValue<float>()} force");
+                    handAnimator.SetFloat(finger, thumbPressAction.action.ReadValue<float>());
+                    continue;
+                }
+
+                if (finger is nameof(Fingers.Index))
+                {
+                    Debug.Log($"I'm pressing trigger {triggerPressAction.action.ReadValue<float>()} force");
+                    handAnimator.SetFloat(finger, triggerPressAction.action.ReadValue<float>());
+                    continue;
+                }
+                
                 if (gripPressAction.action.ReadValue<float>() != 0)
                 {
                     handAnimator.SetFloat(finger, gripPressAction.action.ReadValue<float>());
                     continue;
                 }
+                
+ 
                 handAnimator.SetFloat(finger, 0);
                
                 
