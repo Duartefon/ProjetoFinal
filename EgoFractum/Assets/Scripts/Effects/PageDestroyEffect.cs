e99b408743bf80ecc7e2ba22ae87a7b4429de82e
Gabriel <gabrielrei2112@gmail.com>
2026-06-26 23:50:36 +0100

    trying page breaking effect

---
+++ EgoFractum/Assets/Scripts/Effects/PageDestroyEffect.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PageDestroyEffect : MonoBehaviour
+{
+    [Header("References")]
+    [Tooltip("O Rigidbody do objeto dinâmico sob as páginas")]
+    public Rigidbody objectRigidbody;
+    public ParticleSystem pageParticleSystem;
+    public AudioSource audioSource;
+
+    [Header("Settings")]
+    [Tooltip("O quão rápido o Rigidbody tem de se mexer para as páginas serem destruídas")]
+    public float movementThreshold = 0.5f;
+
+    
+    void Update()
+    {
+        if(objectRigidbody != null && objectRigidbody.linearVelocity.magnitude > movementThreshold)
+        {
+            DestroyPages();
+        }
+    }
+
+    private void DestroyPages()
+    {
+        if(pageParticleSystem != null) Instantiate(pageParticleSystem, transform.position, Quaternion.identity);
+        if(audioSource != null) AudioSource.PlayClipAtPoint(audioSource.clip, transform.position, audioSource.volume);
+        Destroy(gameObject);
+    }
+}
