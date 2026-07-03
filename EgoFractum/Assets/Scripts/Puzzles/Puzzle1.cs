bc66439beddd554655ea00bde1bc74767f0a0065
Duartefon <39652769+Duartefon@users.noreply.github.com>
2026-07-03 12:24:39 +0100

    Feat: Puzzle1 Keypad done

--- EgoFractum/Assets/Scripts/Puzzles/Puzzle1.cs
+++ EgoFractum/Assets/Scripts/Puzzles/Puzzle1.cs
@@ -4,12 +4,14 @@ public class Puzzle1 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    [SerializeField] private string keyCode = "9347" ;
+    [SerializeField] private string keyCode = "" ;
+    [SerializeField] private TMPro.TMP_Text screenText;
     private string _inputKeyCode = string.Empty;
     private bool _puzzleFinished = false;
     void Start()
     {
-        
+        SetText( "CODIGO");
+         
     }
 
     // Update is called once per frame
@@ -21,23 +23,17 @@ public class Puzzle1 : MonoBehaviour
     public void OnKeyPressed(string key)
     {
         Debug.Log($"KeyPressed: {key}");
+        
         _inputKeyCode += key;
-        if (_inputKeyCode == keyCode)
-            _puzzleFinished = true;
-        else if (_inputKeyCode.Length >= 4)
-        {
-            Debug.Log("Wrong Keycode");
-        }
-
+        
+        SetText( _inputKeyCode);
+        
+        
+     
     }
     public void OnEnterPressed()
     { 
-        if (_inputKeyCode == keyCode)
-            _puzzleFinished = true;
-        else if (_inputKeyCode.Length >= 4)
-        {
-            Debug.Log("Wrong Keycode");
-        }
+        VerifyKeyCode();
 
     }
     
@@ -45,6 +41,29 @@ public class Puzzle1 : MonoBehaviour
     { 
         //get string from start to last position-1, therefore excluding last char
         _inputKeyCode = _inputKeyCode.Substring(0, _inputKeyCode.Length - 1);
+        SetText(_inputKeyCode);
 
+    }
+
+    private void VerifyKeyCode()
+    {
+        if (_inputKeyCode.Equals(keyCode))
+        {
+            _puzzleFinished = true;
+            
+            Debug.Log("Puzzle finished!");
+            SetText("Success!");
+            
+        }
+        else
+        {
+            _inputKeyCode = "";
+        }
+        
+    }
+
+    private void SetText(string text)
+    {
+        screenText.text = text;
     }
 }
