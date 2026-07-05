47ed10454bd0bfc2952b412946f67ee55f15068d
Kunz <39652769+Duartefon@users.noreply.github.com>
2026-07-06 00:23:26 +0100

    chore: transitionBeam cleanUp

--- EgoFractum/Assets/Scripts/Gameplay/TransferBeam.cs
+++ EgoFractum/Assets/Scripts/Gameplay/TransferBeam.cs
@@ -1,47 +1,61 @@
-using System;
 using System.Collections;
 using ScriptableObjects;
 using UnityEngine;
 
-public class TransferBeam : MonoBehaviour
+namespace Gameplay
 {
-    [SerializeField] private Transform _raycastOrigin;
-    [SerializeField] private float _rayLength = 3f;
-    [SerializeField] private LayerMask layerMask;
-    public Material m;
-    [SerializeField]  private GameObject miniPlayer;
-    [SerializeField] private GameObject player;
-
-    [SerializeField] TransitionEffectManager _effectManager;
-    [SerializeField] private PlayerTransferData playerData;
-    [SerializeField] private PlayerTransferData miniPlayerData;
-
-    private Coroutine _coroutine;
-    private void Update()
+    public class TransferBeam : MonoBehaviour
     {
-       var didHit=  Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward * _rayLength, out var hit, layerMask );
-        Debug.DrawRay(_raycastOrigin.position, _raycastOrigin.forward * _rayLength);
-        if (didHit && hit.transform.gameObject.CompareTag("Transfer"))
+        [SerializeField] private Transform _raycastOrigin;
+        [SerializeField] private float _rayLength = 3f;
+        [SerializeField] private LayerMask layerMask;
+        public Material m;
+        [SerializeField] private GameObject player;
+
+        [SerializeField] TransitionEffectManager _effectManager;
+        [SerializeField] private PlayerTransferData playerData;
+        [SerializeField] private PlayerTransferData miniPlayerData;
+
+        private enum TransferState
         {
-            if (_coroutine != null) return;
+            PlayerToPuzzle,
+            PuzzleToPlayer
+        }
+        private Coroutine _coroutine;
+        private void Update()
+        {
+            var didHit=  Physics.Raycast(_raycastOrigin.position, _raycastOrigin.forward * _rayLength, out var hit, layerMask );
+            Debug.DrawRay(_raycastOrigin.position, _raycastOrigin.forward * _rayLength);
+            if (didHit && hit.transform.gameObject.CompareTag("Transfer"))
+            {
+                if (_coroutine != null) return;
                 _coroutine = StartCoroutine(OnTransfer());
+            }
+
         }
 
-    }
-
-    IEnumerator OnTransfer()
-    {
-        _effectManager.PlayEffect("playTransition");
+        IEnumerator OnTransfer(TransferState transferState)
+        {
+            _effectManager.PlayEffect("playTransition");
+            yield return new WaitForSeconds(_effectManager.effectTime*2); 
         
-        yield return new WaitForSeconds(_effectManager.effectTime*2);
-        player.transform.position = miniPlayerData.position;
-        player.transform.eulerAngles = miniPlayerData.rotation;
-        player.transform.localScale = miniPlayerData.scale;
-
-        player.GetComponent<CharacterController>().stepOffset = miniPlayerData.stepOffset;
+            if(transferState.Equals(TransferState.PlayerToPuzzle))
+                TransitionPlayerTo(miniPlayerData);
+            else if (transferState.Equals(TransferState.PuzzleToPlayer))
+                TransitionPlayerTo(playerData);
 
 
+        }
+
+        private void TransitionPlayerTo(PlayerTransferData plData)
+        {
+            player.transform.position = miniPlayerData.position;
+            player.transform.eulerAngles = miniPlayerData.rotation;
+            player.transform.localScale = miniPlayerData.scale;
+
+            player.GetComponent<CharacterController>().stepOffset = miniPlayerData.stepOffset;
+        }
+    
+    
     }
-    
-    
 }
