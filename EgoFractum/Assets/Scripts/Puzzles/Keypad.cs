0538dec13513e7118df8d108ed8ef66dc511dcb5
Kunz <39652769+Duartefon@users.noreply.github.com>
2026-07-08 22:45:11 +0100

    Fix: Keypad Buttons, logic and HUD

--- EgoFractum/Assets/Scripts/Puzzles/Keypad.cs
+++ EgoFractum/Assets/Scripts/Puzzles/Keypad.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class Keypad : MonoBehaviour
@@ -7,17 +8,18 @@ public class Keypad : MonoBehaviour
     [SerializeField] private TMPro.TMP_Text screenText;
     private string _inputKeyCode = string.Empty;
     private bool _puzzleFinished = false;
+    
 
     void Start()
     {
-        SetText("****");
+       
     }
 
     public void OnKeyPressed(string value)
     {
         Debug.Log($"KeyPressed: {value}");
-
-        _inputKeyCode += value;
+        if(_inputKeyCode.Length < keyCode.Length)
+            _inputKeyCode += value;
 
         SetText(_inputKeyCode);
     }
@@ -30,7 +32,10 @@ public class Keypad : MonoBehaviour
     public void OnClearPressed()
     {
         //get string from start to last position-1, therefore excluding last char
-        _inputKeyCode = _inputKeyCode.Substring(0, _inputKeyCode.Length - 1);
+        
+        if(_inputKeyCode.Length > 0)
+            _inputKeyCode = _inputKeyCode.Substring(0, _inputKeyCode.Length - 1);
+        
         SetText(_inputKeyCode);
     }
 
@@ -40,16 +45,18 @@ public class Keypad : MonoBehaviour
         {
             _puzzleFinished = true;
             PuzzleManager.Instance.CompletePuzzle(puzzleKey);
-            Debug.Log("Puzzle finished!");
+            SetText("Correct!");
+            
         }
         else
         {
             _inputKeyCode = "";
+            SetText("");
         }
     }
 
     private void SetText(string text)
     {
-        screenText.text = text;
-    }
+        screenText.text = text.Length <= 0 ? "****" : text;
+        }
 }
